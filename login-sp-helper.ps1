745323781144dddb51fe21a25482186ce2aa5a5e
Jon Satchwell <josatchw@microsoft.com>
2020-05-28 11:42:16 +0200

    renamed file and added some comments

--- login-sp-helper.ps1
+++ login-sp-helper.ps1
@@ -1,6 +1,21 @@
+# The following script can retrieve options for serice principal credentials from a sinlge key vault. Each secret will be listed to choose from as a login
+# Make sure that your main Azure identity, the one you log in to Azure with first using Connect-AZAccount, has read-write permission on the kv.
+# The SP creds need to be stored as a single secret using the following json structure.  See setup-sp.ps1 to create the SP and get the json
+# # SP credentials structure
+# {
+#     "clientId": "xxxxxxxxx-de6f-4d16-933f-xxxxxxxxx",
+#     "displayName": "AzOps",
+#     "name": "xxxxxxxxx-de6f-4d16-933f-xxxxxxxxx",
+#     "clientSecret": "xxxxxxxxx-yyyyyy-xxxxxxxxx-zzzzz",
+#     "tenantId": "xxxxxxxxx-722d-4429-b82e-xxxxxxxxx",
+#     "subscriptionId": "xxxxxx-b647-4299-9014-xxxxxxxx"
+#   }
+ 
+#Connect to Azure as your main identity
 Clear-AzContext
 Connect-AzAccount
 
+# Use a service principal available to you from a key vault
 $selection = Read-Host -Prompt "Login with Service Principal? 1=yes"
 $selection=1
 if ($selection -eq 1) {
